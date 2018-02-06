2cbd3f2f9b65e65c854aac94fb416694264fa7e2
SirMeowingtons5 <tarannikov.va+github@gmail.com>
2018-02-06 13:43:52 +0300

    Added Model class

---
+++ src/main/java/model/Model.java
@@ -0,0 +1,74 @@
+package model;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+/**
+ * Class for model instance
+ * @author Viacheslav Tarannikov
+ * @version 0.1a
+ */
+public class Model {
+    private double timeStart;
+    private double timeEnd;
+    private ArrayList<Column> columns = new ArrayList<>();
+    private ArrayList<Frame> frames = new ArrayList<>();
+
+    /**
+     * Constructor
+     * @param timeStart - start time of the model
+     * @param timeEnd - end time of the model
+     */
+    public Model(double timeStart, double timeEnd){
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+    }
+
+    /**
+     * @see Model#timeStart
+     * @return Starting time for current model
+     */
+    public double getTimeStart(){
+        return timeStart;
+    }
+
+    /**
+     * @see  Model#timeEnd
+     * @return End time for current model
+     */
+    public double getTimeEnd(){
+        return timeEnd;
+    }
+
+    /**
+     * Adds column to current model
+     * @param column - instance of <b>Column</b>
+     */
+    public void addColumn(@NotNull Column column){
+        columns.add(column);
+    }
+
+    /**
+     * @return all columns of existing model
+     */
+    public ArrayList<Column> getColumns() {
+        return columns;
+    }
+
+    /**
+     * Adds frame to current model
+     * @param frame - instance of <b>Frame</b>
+     */
+    public void addFrame(@NotNull Frame frame){
+        frames.add(frame);
+    }
+
+    /**
+     * @return all frames of existing model
+     */
+    public ArrayList<Frame> getFrames() {
+        return frames;
+    }
+}
